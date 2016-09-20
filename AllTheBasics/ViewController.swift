79b6db2f0b63891a9ecdfc5ddc82f1c46edcff49
Blake Minho Kim <blakeminhokim@Blakes-MacBook-Pro.local>
2016-09-20 00:51:59 -0400

    finished, couldn't run because Flatiron code problems. Not me.

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -15,7 +15,54 @@ class ViewController: UIViewController {
     }
     
     
-   // Implement your functions here
+    func averageIsAbove75 (a: Double, b: Double, c: Double) -> Bool {
+        let d = (a + b + c)/3
+        if d > 75.0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    func passwordCombo (var1: String, var2: Int) -> String{
+        let result = var2%3
+        
+        if var1 == "Jerry" && result == 0 {
+            return "Welcome!"
+        } else if var1 == "Elaine" && result == 0{
+            return "Welcome!"
+        } else if var1 == "Michael" && result == 0{
+            return "Welcome!"
+        } else {
+            return "Access Denied"
+        }
+    }
+    
+    
+    func describe(emoji: String) -> String {
+        switch emoji {
+        case "Kiss":
+            return "Kiss"
+            
+        case "Cat":
+            return "Cat"
+            
+        case "Turtle":
+            return "Turtle"
+            
+        case "Pizza":
+            return "Pizza"
+            
+        case "Ghost":
+            return "Unknown"
+            
+        default:
+            return "Unknown"
+            
+        }
+    }
+
 
 
 }
